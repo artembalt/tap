4d94c625039ecb28108b4eb01d7562a213defc25
artembalt <artembalt@gmail.com>
2026-01-17 00:50:01 +0300

    Auto-sync 2026-01-17 00:50

---
+++ bot/config/__init__.py
@@ -0,0 +1,11 @@
+# bot/config/__init__.py
+"""Конфигурация бота"""
+
+from bot.config.pricing import (
+    ACCOUNT_TYPES,
+    PAID_SERVICES,
+    STARS_CONFIG,
+    PROMO_TYPES,
+    get_service_price,
+    get_account_limits,
+)
